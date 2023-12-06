b028a2cda23472bc61fd14b93329e4f2f9a2b91d
dharlabwustl <atulkumar@wustl.edu>
2023-12-05 21:35:25 -0600

    First commit

--- javafunction2.js
+++ javafunction2.js
@@ -1,2 +1,2 @@
-
+filltable();
 displayImage(array_0[initial_slice_with_lesion],array_1[initial_slice_with_lesion], array_2[initial_slice_with_lesion],array_3[initial_slice_with_lesion],  imagewidth, imageheight);
